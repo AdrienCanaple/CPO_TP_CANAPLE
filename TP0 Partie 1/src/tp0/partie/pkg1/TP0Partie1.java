ee8bbef47a7e593a63fd12f694371f69e0278c91
adrie <adrie@10.3.61.215>
2024-09-17 12:44:59 +0200

    avancement tp p6

--- TP0 Partie 1/src/tp0/partie/pkg1/TP0Partie1.java
+++ TP0 Partie 1/src/tp0/partie/pkg1/TP0Partie1.java
@@ -1,6 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/* 
+ * Adrien CANAPLE
+ * TP0 partie 1
+ * TPB
+ * 17/09/2024
  */
 package tp0.partie.pkg1;
 
@@ -14,7 +16,7 @@ public class TP0Partie1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour");
+        
     }
-    
 }
