e10327c403e49c55049a6a835dbbadeef421bdb8
adrie <adrie@10.3.61.215>
2024-10-11 09:24:50 +0200

    TP2 Exo1 fini

--- TP2_Bieres_CANAPLE/src/tp2_bieres_canaple/BouteilleBiere.java
+++ TP2_Bieres_CANAPLE/src/tp2_bieres_canaple/BouteilleBiere.java
@@ -12,11 +12,35 @@ package tp2_bieres_canaple;
 public class BouteilleBiere {
     
     String Nom;
-    float degreAlcool;
+    double degreAlcool;
     String brasserie;
     boolean ouverte;
-    
+
 public void lireEtiquette () {
-    System.out.println("Bouteille de " + Nom + " degreAlcool "+ degreAlcool +"\n"+"Brasserie : " + brasserie ) ;
+    System.out.println("Bouteille de " + Nom +"\n"+ "degré d'alcool "+ degreAlcool +"\n"+"Brasserie : " + brasserie ) ;
+}
+public void Décapsuler (){
+    if(ouverte == false){
+        System.out.println("la biere est ouverte");
+        ouverte=true;
+}
+    else{
+        System.out.println("Erreur : la bière est déja ouverte");
+    }
+}
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    Nom = unNom;
+    degreAlcool = unDegre;
+    brasserie = uneBrasserie;
+    ouverte = false;
+}
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = Nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
 }
 }
+
