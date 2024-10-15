1cfed50cd06c4c30d930c45e7ad37e5638f9cb82
adrie <adrie@10.3.61.215>
2024-10-15 17:12:15 +0200

    TP2 Exo2 début

---
+++ TP2_convertisseurObjet_CANAPLE/src/tp2_convertisseurobjet_canaple/TP2_convertisseurObjet_CANAPLE.java
@@ -0,0 +1,21 @@
+/*
+ *TP2 Exo convertisseur
+ * Adrien CANAPLE
+ * 15/10/2024
+ */
+package tp2_convertisseurobjet_canaple;
+
+/**
+ *
+ * @author adrie
+ */
+public class TP2_convertisseurObjet_CANAPLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
