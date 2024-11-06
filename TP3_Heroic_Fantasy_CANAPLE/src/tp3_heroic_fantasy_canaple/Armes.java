7d464e859ee81828a3db770345ee5bb8927d7966
adrie <adrie@10.3.61.215>
2024-11-06 17:10:35 +0100

    TP3 exo 1

--- TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/Armes.java
+++ TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/Armes.java
@@ -2,28 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package tp3_heroic_fantasy_canaple;
 
 /**
  *
  * @author adrie
  */
-public class Arme {
+public abstract class Armes {
     String nom;
-    private int niveauAttaque;
+    protected int niveauAttaque;
 
-    public Arme(String nom, int niveauAttaque) {
+    public Armes(String nom, int niveauAttaque) {
         this.nom = nom;
-        if (niveauAttaque <= 100 & niveauAttaque >=0){
+        if (niveauAttaque<=100 & niveauAttaque >=0){
         this.niveauAttaque = niveauAttaque;
         }
-        else {
+        else{
             this.niveauAttaque = 100;
         }
-        
     }
 
     public int getNiveauAttaque() {
         return niveauAttaque;
     }
+
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", niveauAttaque=" + niveauAttaque + '}';
+    }
+    
     
 }
--- TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/TP3_Heroic_Fantasy_CANAPLE.java
+++ TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/TP3_Heroic_Fantasy_CANAPLE.java
@@ -15,6 +15,9 @@ public class TP3_Heroic_Fantasy_CANAPLE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Armes pistolet = new Armes ("pistolet", 100);
+        System.out.println(pistolet);
     }
     
 }
