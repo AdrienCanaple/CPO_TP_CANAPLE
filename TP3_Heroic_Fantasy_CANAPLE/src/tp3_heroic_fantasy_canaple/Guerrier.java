d3c7c4aa77e929b9669083f57c48b3322e215859
adrie <adrie@10.3.61.215>
2024-11-13 16:37:35 +0100

    TP3 exo 2

---
+++ TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/Guerrier.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_canaple;
+
+/**
+ *
+ * @author adrie
+ */
+public class Guerrier extends personnages {
+    boolean cheval;
+
+    public Guerrier(String nom, int niveauDeVie, boolean cheval) {
+        super(nom, niveauDeVie);
+        this.cheval = cheval;
+    }
+
+    @Override
+    public String toString() {
+        return "Guerrier{ nom = "+nom+ " niveau de vie = "+ niveauDeVie + "cheval=" + cheval + '}';
+    }
+
+    public void setCheval(boolean cheval) {
+        this.cheval = cheval;
+    }
+}
---
+++ TP3_Heroic_Fantasy_CANAPLE/src/tp3_heroic_fantasy_canaple/Magicien.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_canaple;
+
+/**
+ *
+ * @author adrie
+ */
+public class Magicien extends personnages {
+    boolean confirmé;
+
+    public Magicien(String nom, int niveauDeVie, boolean confirmé) {
+        super(nom, niveauDeVie);
+        this.confirmé = confirmé;
+    }
+
+    @Override
+    public String toString() {
+        return "Magicien{ nom = "+ nom +" niveau de vie = "+ niveauDeVie + " confirmé = " + confirmé + '}';
+    }
+
+    public void setConfirmé(boolean confirmé) {
+        this.confirmé = confirmé;
+    }
+}
